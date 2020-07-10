1660df594a05927fa0e81a03c9acc77a57ee746f
Joe <jli@spartaglobal.com>
2020-07-10 15:27:32 +0100

    nested if statements

--- Introduction/hello_if.py
+++ Introduction/hello_if.py
@@ -11,17 +11,31 @@
 
 # U, PG, 12A, 15, 18
 
-movie = input("What type of movie do you want to see? (u, pg, 12, 12a, 15, 18) \n")
+# movie = input("What type of movie do you want to see? (u, pg, 12, 12a, 15, 18) \n")
+#
+# if movie.lower() == "u":
+#     print("All ages can see this movie")
+# elif movie.lower() == "pg":
+#     print("you need parental guidance when seeing this movie")
+# elif movie.lower() == "12a" or movie.lower() == "12":
+#     print("you need to be 12 or above and accompanied by an adult")
+# elif movie.lower() == "15":
+#     print("you need to be 15 years old or above to see this movie")
+# elif movie.lower() == "18":
+#     print("you need to be 18 years old or above to see this movie")
+# else:
+#     print("enter a valid film rating")
 
-if movie.lower() == "u":
-    print("All ages can see this movie")
-elif movie.lower() == "pg":
-    print("you need parental guidance when seeing this movie")
-elif movie.lower() == "12a" or movie.lower() == "12":
-    print("you need to be 12 or above and accompanied by an adult")
-elif movie.lower() == "15":
-    print("you need to be 15 years old or above to see this movie")
-elif movie.lower() == "18":
-    print("you need to be 18 years old or above to see this movie")
-else:
-    print("enter a valid film rating")
+# age = 6
+# adult_present = True
+#
+# if age > 12:
+#     print("come on in")
+# else:
+#     if adult_present:
+#         print("come on in")
+#     else:
+#         print("get an adult")
+
+
+# FOR LOOPS
