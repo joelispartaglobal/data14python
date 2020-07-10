c4a6d791abffc71473c729ef4b54413ad64061cc
Joe <jli@spartaglobal.com>
2020-07-10 14:45:47 +0100

    dictionaries and tuples

--- Introduction/hello_collections.py
+++ Introduction/hello_collections.py
@@ -30,11 +30,70 @@
 # pop_return = shopping_list.pop()
 # print(pop_return)
 
-# Tuples
+# # Tuples
+#
+# colours = ("blue", "purple", "turquoise")
+# print(colours)
+# print(type(colours))
+# print(colours[-1])
+#
+# # TUPLES ARE IMMUTABLE - CANNOT BE CHANGED
+#
+# print(colours.count("orange"))
+# print(colours.index("purple"))
+#
 
-colours = ("blue", "purple", "turquoise")
-print(colours)
-print(type(colours))
-print(colours[-1])
+# DICTIONARIES
 
-# TUPLES ARE IMMUTABLE - CANNOT BE CHANGED
+# my_dict = {"key": "Value"}
+# print(my_dict)
+# print(type(my_dict))
+#
+# bigger_dictionary = {
+#     "key1": "value1",
+#     "key2": "value2",
+#     "key3": "value3"
+# }
+
+# my_dictionary = {
+#     "name": "Joe",
+#     "age": 24,
+#     "birthday": "03/10/95",
+#     "Favourite colour": "blue",
+#     "favourite animal": "raccoon",
+#     "favourite music": "house"
+# }
+#
+# print(my_dictionary)
+#
+# print(my_dictionary["name"])
+# # dict[key]
+#
+# my_dictionary["favourite music"] = "Trance"
+# my_dictionary["new_key"] = "Some new value"
+#
+# print(my_dictionary)
+#
+# del my_dictionary["new_key"]
+#
+# print(my_dictionary)
+# print(my_dictionary.keys())
+# print(my_dictionary.values())
+
+data14_dictionary = {
+    "trainer": "David Harvey",
+    "location": "Birmingham",
+    "course": "data engineering",
+    "duration": "12 weeks",
+    "topics": ["business skills", "sql", "critical thinking", "agile", "python", "big data", "nosql", "cloud", "project"],
+    "schedule": {
+        "week 1": "Business week",
+        "week 2": "SQL"
+    },
+    "number of participants": "11"
+
+}
+
+print(data14_dictionary)
+
+print(data14_dictionary["topics"][4])
