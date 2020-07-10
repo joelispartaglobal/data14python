cf3878c089ab70e03a46164ab9ad2a98eb27dc1f
Joe <jli@spartaglobal.com>
2020-07-10 12:09:15 +0100

    added stuff to hello world and hello variables

---
+++ Introduction/hello_variables.py
@@ -0,0 +1,96 @@
+# a = 5
+# b = "hello"
+# c = True
+# d = 0.25
+# e = (0, 2, 3)
+#
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+# print(e)
+#
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+# print(type(e))
+#
+
+# print("What is your name?")
+# name = "Joe"
+#
+# print("Hello...")
+# print(name)
+
+# name = input("What is your name? \n")
+# age = input("How old are you? \n")
+# dob = input("What is your date of birth? \n")
+#
+# print("Hello")
+# print(name)
+# print(age)
+# print(dob)
+#
+# print(type(dob))
+# # it is a string
+
+# print(1+1)
+# # print(1-2)
+# # print(14 % 3)
+
+# > greater than
+# < less than
+# >= greater or equal to
+# <= lesser or equal to
+# == must equal; identical
+# != does not equal
+
+# NUMBERS
+# print(0.25)  # float
+# print(1)  # int
+# print(complex(1, 2))  # complex
+
+# STRINGS
+# single = 'single quotes'
+# double = "double quotes"
+#
+# print(single, double)
+
+# hw = "Hello world"
+# print(hw)
+# print(hw[2:])
+# print(hw[:7])
+# print(hw[-4])
+# print(hw[-1:])
+# print(len(hw))
+
+# white_space = "     here is a string with a bunch of white space"
+# print(len(white_space))
+# print(white_space)
+# print(white_space.strip())
+# print(white_space.lstrip())
+# print(white_space.rstrip())
+#
+# print(white_space.count(" "))
+# print(white_space.lower())
+# print(white_space.upper())
+# # Capitalise() will auto capitalise a string
+# print(white_space.replace("space", "noise"))
+# print(white_space.upper().replace("space", "noise"))
+
+# a = "Hello"
+# b = "World"
+# print(a + ' ' + b)
+#
+# print(f"{a}, {b}")
+#
+# print(f"I am printing {a} {b}")
+
+name = input("What is your name? \n")
+age = input("How old are you? \n")
+sibling = input("How many siblings do you have? \n")
+dec_num = input("What is your favourite decimal number? \n")
+animal = input("What is your favourite animal? \n")
+
+print(f"My name is {name}, I am {age} years old and I have {sibling} siblings. My favourite decimal number is {dec_num} and my favourite animal is {animal}")
