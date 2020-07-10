bc444a9468848f0c16cea69dd062b8215f6b9148
Joe <jli@spartaglobal.com>
2020-07-10 16:18:09 +0100

    while loops and fizzbuzz homework

---
+++ Introduction/fizzbuzz.py
@@ -0,0 +1,16 @@
+# FIZZBUZZ
+
+# start at 1
+# print each number in turn
+# end at 100
+
+# if divisible by 3, instead of a number, "fizz"
+# if divisible by 5, instead of a number, "buzz"
+# if divisible by 3 and 5, instead of a number, "fizzbuzz"
+
+# accept user input for number to count up to
+# accept user input for number to start from
+
+# accept user input for custom fizz buzz?
+# change the number we increment by?
+# change the values of fizz and buzz?
---
+++ Introduction/hello_while.py
@@ -0,0 +1,25 @@
+# counter = 0
+#
+# while counter < 10:
+#     print(f"still counting! {counter}")
+#     if counter == 6:
+#         break
+#     counter += 1
+#
+# print("we've escaped the while loop")
+
+# for number in range(10):
+#     print(number)
+
+age_input = ""
+
+while not age_input.isnumeric():
+    age_input = input("Enter your age:\n")
+    if not age_input.isnumeric():
+        print("That's not a number, try again")
+
+
+age = int(age_input)
+
+
+print("You are" + ' ' + str(age))
