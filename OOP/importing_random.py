591445007cc3f2c890f160b20472a2870b2fb7c6
Joe <jli@spartaglobal.com>
2020-07-13 15:57:50 +0100

    added hangman game file

---
+++ OOP/importing_random.py
@@ -0,0 +1,14 @@
+# import random as rand
+# from classes import Dog
+#
+# print(rand.randint(0,6))
+#
+# my_list = [1, 2, 3, 4, 5, 6]
+#
+# print(rand.choice(my_list))
+#
+# new_dog = Dog("Fido")
+# print(new_dog.intro())
+
+from hangman_words import word_list
+from random import choice
