a69c2906e9bd831b16b75ac45b3919da62a2a553
Joe <jli@spartaglobal.com>
2020-07-15 14:17:47 +0100

    filter function

--- advanced_functions/hello_filter.py
+++ advanced_functions/hello_filter.py
@@ -1,2 +1,16 @@
-numbers = list(range(1, 100))
+students = ["DAVID", "Lee", "RICHARD"]
 
+result = filter(str.isupper, students)
+# Returns anything that is true i.e. the first parameter
+# And removes anything that is false
+
+print(list(result))
+
+# Write a function that returns true if even and divisible by 3
+
+def division(a):
+    return a % 2 == 0 and a % 3 == 0
+
+numbers = list(range(100))
+
+print(list(filter(division, numbers)))
---
+++ advanced_functions/hello_lambdas.py
@@ -0,0 +1,9 @@
+# Lambda is a way of doing anonymous functions
+
+# Without having to define the function in advance
+# Alas doing it on the fly
+
+def add(n1, n2):
+    return n1+n2
+
+print(add(2, 2))
