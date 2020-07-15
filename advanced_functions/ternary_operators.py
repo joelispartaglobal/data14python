282b5fafde2a4b7146ec01cfa15bdb2cff84a1ea
Joe <jli@spartaglobal.com>
2020-07-15 14:38:04 +0100

    ternary operators

--- advanced_functions/ternary_operators.py
+++ advanced_functions/ternary_operators.py
@@ -1,5 +1,16 @@
 # Ternary operators
 # Conditional expressions
 
-age = 17
+# age = 17
+#
+# print("Go ahead" if age >= 18 else "Go away")  # A lot more clearer
+#
+# print(("Go away", "Go ahead")[age >= 18])  # Don't do this
+#
+# # False == 0
+# # True == 1
+# # (tuple)[index]
+#
+# status = "Go ahead!" if age >= 18 else "Go away"
+# print(status)
 
