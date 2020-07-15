2c25796494080b0c43cee63dea807d068eddf162
Joe <jli@spartaglobal.com>
2020-07-15 22:01:44 +0100

    done score and tile generation

--- unit_testing/scrabble.py
+++ unit_testing/scrabble.py
@@ -1,5 +1,6 @@
 import random as rand
 
+
 score = {"a": 1, "b": 3, "c": 3, "d": 2,
          "e": 1, "f": 4, "g": 2, "h": 4,
          "i": 1, "j": 8, "k": 5, "l": 1,
@@ -8,11 +9,16 @@ score = {"a": 1, "b": 3, "c": 3, "d": 2,
          "u": 1, "v": 4, "w": 4, "x": 8,
          "y": 4, "z": 10}
 
+words = ["direct", "nosy", "surmise", "slip", "fabulous"]
+tiles = ["o", "s", "y", "n", "f", "u", "s", "r"]
+
 class ScrabbleGame:
 
-    # def generatetiles(self):
-    #     tilebag = []
-    #     for i in range(score)
+    def tilegeneration(self):
+        return rand.sample(list(score), 7)
+
+    def choose_word(self):
+        return rand.choice(list(words))
 
     def scrabblescore(self, word):
         total = 0
@@ -21,5 +27,4 @@ class ScrabbleGame:
         return total
 
 
-test_game = ScrabbleGame()
-print(test_game.scrabblescore("dog"))
+test = ScrabbleGame()
--- unit_testing/test_scratch.py
+++ unit_testing/test_scratch.py
@@ -1,15 +1,6 @@
-# TEST DRIVEN ENVIRONMENT
-# Import the file
-import pytest
 from scrabble import ScrabbleGame
 my_scrabble_game = ScrabbleGame()
 
-
-# Test file
-# We need an iterative method to calculate the score
-# A list of the scores
-
-# test_list = {"dialogue", "grass", "nomination", "low", "disgrace"}
 
 def test_scrabble_score_1():
     assert my_scrabble_game.scrabblescore("dog") == 5
@@ -21,20 +12,27 @@ def test_scrabble_score_3():
     assert my_scrabble_game.scrabblescore("hello") == 8
 
 def test_scrabble_score_4():
-    assert my_scrabble_game.scrabblescore("low") == 6
+    assert my_scrabble_game.scrabblescore("something") != 0
 
 def test_scrabble_score_5():
-    assert my_scrabble_game.scrabblescore("testing") == 8
+    assert my_scrabble_game.scrabblescore("hello") != 23123
 
 def test_scrabble_score_6():
-    assert my_scrabble_game.scrabblescore("") == 0
+    assert my_scrabble_game.scrabblescore("cat") != 2
 
-# Write unit tests first!
+def test_tile_generation():
+    assert len(my_scrabble_game.tilegeneration()) == 7
+
+def test_tile_generation_2():
+    assert len(my_scrabble_game.tilegeneration()) != 6
+
+def test_tile_generation_3():
+    assert len(my_scrabble_game.tilegeneration()) != 8
 
 # Class!
 
 # Scrabble score calculator done
 # Return the score for a word according to scrabble letter points done
-# Randomly generate 7 tile hand
-# Check that the word can be made using those tiles
-# Once a word has been made, replace used tiles
+# Randomly generate 7 tile hand done
+# Check that the word can be made using those tiles still need doing
+# Once a word has been made, replace used tiles still need doing
