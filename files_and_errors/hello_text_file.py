7c41ac4d755c9521ee85526a589197420e1909fe
Joe <jli@spartaglobal.com>
2020-07-16 11:28:32 +0100

    text types and order.txt

---
+++ files_and_errors/hello_text_file.py
@@ -0,0 +1,28 @@
+# file = open("order.txt")
+# No such file or directory
+
+# Syntax is the grammar of programming languages
+# The above is missing a colon
+
+# ZeroDivison error
+# Indentation error
+# Syntax error
+# Attribute error
+
+# try:
+#     print("Trying to open the file...")
+#     file = open("order.txt")
+#     print("Successfully opened the file...")
+# except FileNotFoundError as errmsg:
+#     print("Error! PANIC!")
+#     print(errmsg)
+#     raise
+# finally:
+#     print("Finished handling everything")
+#
+# print("This is the end of the file")
+#
+# # Can write multiple exceptions
+# # Can raise an error
+# # Can have a finally section
+
---
+++ files_and_errors/hello_text_files.py
@@ -0,0 +1,62 @@
+# r - Read mode (default)
+# w - write mode (if no file, creates one; if file, truncate)
+# x - create mode (if file, fails)
+# a - append mode (if no file, create one; if file, appends
+# t - text mode
+# b - binary mode
+# + - reading and writing
+
+# def open_and_print_file(filename):
+#     try:
+#         opened_file = open(filename, "r")
+#         file_line_list = opened_file.readlines()
+#
+#         for line in file_line_list:
+#             print(line.rstrip('\n'))
+#
+#         opened_file.close()
+#
+#     except FileNotFoundError:
+#         print("File cannot be found, please check filename provided")
+#         raise
+#
+# file = open("order.txt")
+# print(file.readline())
+# print(file.readline())
+# print(file.readline())
+# # It will read a new line and the original line is lost from the buffer
+#
+# # New text lines come with a new line indicator which is why it produces
+# # Big gaps when running the function
+
+# def open_and_print_file(filename):
+#     try:
+#         with open(filename, 'r') as opened_file:
+#             file_line_list = opened_file.readlines()
+#
+#             for line in file_line_list:
+#                 print(line.rstrip('\n'))
+#
+#     except FileNotFoundError:
+#         print("File cannot be found, please check filename provided")
+#         raise
+#
+# open_and_print_file("order.txt")
+
+# with open("order.txt", "w") as opened_file:
+#     opened_file.write("Cheeseburger")
+#
+
+
+def append_to_file(filename, order):
+    try:
+        with open(filename, "a") as opened_file:
+            opened_file.write(order)
+
+    except FileNotFoundError:
+        print("File cannot be found, please check filename provided")
+        raise
+
+append_to_file("order.txt", "pasta")
+append_to_file("order.txt", "hotdog")
+append_to_file("order.txt", "kebab")
