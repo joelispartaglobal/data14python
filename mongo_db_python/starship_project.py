56ece0f49083405af1cf90d23c5fb63a9c2cab1e
Joe <jli@spartaglobal.com>
2020-07-30 21:59:33 +0100

    another attempt but with functions and pseudocode

---
+++ mongo_db_python/starship_project.py
@@ -0,0 +1,78 @@
+import pymongo
+import json
+import requests
+import sys
+
+# for every starship list of pilots
+# make a request for those pilot to grab name
+# change list of url to name
+# query mongodb character collection based on name
+# grab objectid
+# essentially change list of names into objectids
+
+
+api = "http://swapi.dev/api/starships/?page="
+starship_list = []
+pilot_url_list = []
+pilot_name_list = []
+pilot_object_list = []
+
+def get_starship():
+    # iterate through all pages to get the starship data
+    for i in range(1, 5):
+        url = f"{api}{i}"
+        info = requests.get(url).json()
+        for each in info['results']:
+            starship_list.append(each)
+        print(starship_list)
+
+def load_pilots():
+    # Save all the pilot urls to a list
+    for i in range(1, 5):
+        url = f"{api}{i}"
+        info = requests.get(url).json()
+        for each in info['results']:
+            pilot_url_list.append((each['pilots']))
+        print(pilot_url_list)
+
+def pilot_name():
+    for link in pilot_url_list:
+        if link in pilot_url_list:
+            url = link
+            info = requests.get(url).json()
+            for each in info:
+                pilot_name_list.append((each['name']))
+            print(pilot_name_list)
+
+def pilot_object():
+    # Loop through each pilot name
+    # for variable in pilot_name_list
+    # variable = db.characters.find({name:"a"}, {_id:1})
+    # append to pilot_object_list
+    return
+
+def join_object_pilot():
+    # append pilot_url_list and pilot_object_list together
+    return
+
+def replace_url():
+    # in starship_list replace the links in the pilot
+    # with the objects
+    return
+
+def create_db():
+    # client = pymongo.MongoClient()
+    # db = client['starwars']
+    # create a new collection starship
+    return
+
+def import_json():
+    # with open() as f:
+    #   file_data = json.load(f)
+    # read dictionary into new collection
+    # db.starship.insert_many(file_data)
+    return
+
+load_pilots()
+pilot_name()
+
--- mongo_db_python/starships.py
+++ mongo_db_python/starships.py
@@ -11,10 +11,14 @@ class Starship:
         self.api = "http://swapi.dev/api/starships/?page="
         self.api_page = []
         self.req_response = requests.get(self.api)
+        self.req_content = self.req_response.content
         self.all_info = self.req_response.json()
         self.starship_list = []
         self.pilot_url_list = []
-        self.pilot_name = []
+        self.pilot_name_list = []
+
+    def info(self):
+        return self.req_content
 
     def load_starship(self):
         # Save starships to a list
@@ -31,29 +35,38 @@ class Starship:
             info = requests.get(url).json()
             for each in info['results']:
                 self.pilot_url_list.append((each['pilots']))
-            return self.pilot_url_list
+
 
     def pilot_name(self):
         # iterate through pilot url list to get pilot name
         for i in self.pilot_url_list:
-            if i in self.pilot_url_list:
+            if self.pilot_url_list[i]:
                 url = f"{self.pilot_url_list[i]}"
                 info = requests.get(url).json()
                 for each in info:
-                    self.pilot_name.append(each["name"])
-                return self.pilot_name
-
+                    self.pilot_name_list.append(each)
+                return self.pilot_name_list
 
     def query_name(self):
         # Take the list of pilot names
         # Iterate through the pilot names
         # For each iteration, use a mongodb query
+        # iterate for a in self.pilot_name_list
+        # variable = db.characters.find({name:"a"}, {_id:1})
+        # append to a list that has the object
+        return
 
+    def replace_url(self):
+        # iterate through the name of each starship list
+        #
         return
 
 
+
+
+
 test = Starship()
-print(test.pilot_name)
+print(test.pilot_name())
 
 
 # for every starship list of pilots
